594988328a645a51138edc85f54a5f338a656224
LastBreeze <lastbreezefirst@gmail.com>
2026-01-24 07:51:24 -0900

    Update Auto-Boost-Essential.py fix typo (#23)
    Remove extra )

--- Auto-Boost-Essential/Auto-Boost-Essential.py
+++ Auto-Boost-Essential/Auto-Boost-Essential.py
@@ -866,7 +866,7 @@ def calculate_metric() -> None:
             raise SystemExit(1)
     elif ssimu2 == "gpu":
         try:
-            result = core.vship.SSIMULACRA2(cut_source_clip, cut_encoded_clip, numStream = 2))
+            result = core.vship.SSIMULACRA2(cut_source_clip, cut_encoded_clip, numStream = 2)
         except:
             console.print(f"[red]Vship not found. Check your installation.")
             raise SystemExit(1)
